a9f8d81c458bef120027e29d9c2b26f4f542c865
Nico Baier <nico.baier@gmail.com>
2024-02-02 19:34:46 -0600

    fix(index.ts): remove unnecessary console.log statement feat(index.ts): add support for handling POST requests to create new links and insert them into the database feat(index.ts): add support for CORS and logging middleware to improve security and debugging capabilities feat(index.ts): add support for parsing node contents and inserting the parsed link into the database feat(index.ts): add support for configuring the server port via command line argument or environment variable feat(index.ts): add support for serving the app using Bun framework and display the server URL on startup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log("Hello via Bun!");
+import { buildDbClient } from "./client";
+import { links } from "../drizzle/schema";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { insertLink, type NewLink, type Link } from "./operations";
+import { nanoid } from "nanoid";
+import { parseNode } from "./parse";
+
+const app = new Hono();
+export const db = buildDbClient();
+
+const origins = [
+  "http://localhost",
+  "https://localhost",
+  "http://app.tana.inc",
+  "https://app.tana.inc",
+];
+
+app.use("*", cors({ origin: origins, allowMethods: ["GET", "POST"] }));
+app.use("*", logger());
+
+app.get("/", (c) => {
+  return c.text("Try POST /");
+});
+
+app.post("/", async (c) => {
+  const body: { nodeContents: string } = await c.req.json();
+  const { nodeContents } = body;
+
+  const parsedLink: Link = parseNode(nodeContents);
+
+  const result = await insertLink({ id: nanoid(), ...parsedLink });
+  console.log(result);
+  return c.text("Added!");
+});
+
+const port = Bun.argv[2] ? Bun.argv[1] : process.env.PORT;
+Bun.serve({ port, fetch: app.fetch });
+
+console.log(`Server running at http://localhost:${port}`);
